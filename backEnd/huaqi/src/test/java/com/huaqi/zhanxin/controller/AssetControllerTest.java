35b7ce0150448d4b649ee1fb823481f9a18828c2
junjiangyue <76471982+junjiangyue@users.noreply.github.com>
2022-06-20 00:56:55 +0800

    token要写在header里面

--- backEnd/huaqi/src/test/java/com/huaqi/zhanxin/controller/AssetControllerTest.java
+++ backEnd/huaqi/src/test/java/com/huaqi/zhanxin/controller/AssetControllerTest.java
@@ -1,44 +1,57 @@
 package com.huaqi.zhanxin.controller;
 
 import com.huaqi.zhanxin.ZhanxinApplication;
+import com.huaqi.zhanxin.entity.Asset;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
 @AutoConfigureMockMvc
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ZhanxinApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AssetControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
-    private MockHttpServletRequest request;
+    private MockHttpServletRequest request = new MockHttpServletRequest();
+    @Autowired
+    private WebApplicationContext wac;
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
 
     @Test
     void userList() {
-        MvcResult mvcResult = null;
         request.addParameter("userId", "1"); //直接添加request参数，相当简单
         try {
-            mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/asset/assetList")
+            MvcResult mvcResult= mockMvc.perform(MockMvcRequestBuilders
+                            .get("/asset/assetList")
                             .accept(MediaType.APPLICATION_JSON)
-                            .param("request", String.valueOf(request)))
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andDo(MockMvcResultHandlers.print())
-                    .andReturn();
+                            .header("token","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyRW1haWwiOiIxMjNAcXEuY29tIiwiZXhwIjoxNjU1NzQzNDQ3LCJ1c2VySWQiOiIxIn0.ozVCzrbjCQz7EkdNbmXALIwMDdeW2wjRRbDt7DY61JI")
+                    ).andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+
             log.info(mvcResult.getResponse().getContentAsString());
+            Thread.sleep(10*1000);
         } catch (Exception e) {
             e.printStackTrace();
         }
